77f4b40d0152c50a41223ffca845526fcfab8b2d
Keshav <workwkeshav@gmail.com>
2026-02-21 18:28:28 +0530

    Change: the tab icon for each page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -5,7 +5,10 @@ import "./checkout/CheckoutPage.css";
 export function CheckoutPage() {
   return (
     <>
+      <link rel="icon" type="image/svg+xml" href="cart-favicon.png" />
+
       <title>Checkout</title>
+
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
