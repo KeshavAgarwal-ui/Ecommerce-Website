bd9dcedbbee99b6a037bc07bbcacf4e4afdd0430
Keshav <workwkeshav@gmail.com>
2026-02-21 19:41:23 +0530

    Replace: class with className in Page404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -12,18 +12,18 @@ export function Page404() {
       <Header />
 
       <div className="page-404-wrapper">
-        <div class="error-container">
-          <h1 class="error-code">404</h1>
+        <div className="error-container">
+          <h1 className="error-code">404</h1>
 
-          <h2 class="error-title">PAGE NOT FOUND</h2>
+          <h2 className="error-title">PAGE NOT FOUND</h2>
 
-          <p class="error-message">
+          <p className="error-message">
             The page you are looking for might have been removed, had its name{" "}
             <br />
             changed or is temporarily unavailable.
           </p>
 
-          <Link to="/" class="home-btn">
+          <Link to="/" className="home-btn">
             HOMEPAGE
           </Link>
         </div>
